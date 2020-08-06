erfanoabdi/android_device_halium_ubport

ubport: init: overriding services that causes problems
Signed-off-by: Danct12 <danct12@disroot.org>
Signed-off-by: Dreemurrs Quality Check <quality@dreemurrs.local>

device.mk
@@ -22,6 +22,7 @@ PRODUCT_USE_DYNAMIC_PARTITION_SIZE := true
 
 # init scripts
 PRODUCT_PACKAGES += \
+    init.disabled.rc \
     init.halium.rc \
     vndk-detect \
     on-post-data.sh

Linux-On-Sdm6Series/device_asus_X00TD

device.mk
@@ -549,7 +549,8 @@ PRODUCT_USE_DYNAMIC_PARTITION_SIZE := true
 
 # init scripts
 PRODUCT_PACKAGES += \
-    init.halium.rc
+    init.halium.rc \
+    init.disabled.rc
 
 PRODUCT_PACKAGES += \
     sensorservice